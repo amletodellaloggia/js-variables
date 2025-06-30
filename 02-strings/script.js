639e9e18bac300f78b0be3221b96e2487abbc3fa
amletodellaloggia <amletodellaloggia@gmail.com>
2025-06-30 13:16:13 +0200

    [upd] 2.

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,11 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = "15";
+let text = "Pippo";
+let bool = "False";
+console.log(number+" "+text+" "+bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
